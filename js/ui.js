dccab48c3f0b51cecc063e36e753a094cdc58e82
osama <osama2002ibrahim@gmail.com>
2020-12-25 20:04:41 +0200

    niceeee

--- js/ui.js
+++ js/ui.js
@@ -1,29 +1,41 @@
 const cardContainer = document.querySelector("#rooms-c");
 
-const addCard = (data, id) => {
-  const newCard = `
+var client = contentful.createClient({
+  space: "18yoosog20ju",
+  accessToken: "0I3QCVzsfU2UZolz1Fp3xA8LG8w5oyiXkBGHapaR8JI",
+});
+client.getEntries().then(function (entries) {
+  // log the title for all the entries that have it
+  console.log(entries);
+  entries.items.forEach(function (entry) {
+    console.log(entry);
+    if (entry) {
+      let recipe = entry.fields;
+
+      let id = "ridk";
+      const newCard = `
   <!-- start king room -->
         <div class="col-md-4 col-sm-6 col-xs-12 ">
 
           <div class="card inmatFade">
             <div class="hover-store text-center">
               <div class="img-store">
-                <img src=${data.urls[0]} class="card-img-top" alt="king room">
+                <img src=https:${recipe.fields.images[0]} class="card-img-top" alt="king room">
               </div>
               <div class="back-img-store ">
                 <h3>السعر بالجنيه </h3>
-                <span class=" kingPrice label label-success">${data.price} الف</span>
+                <span class=" kingPrice label label-success">${recipe.price} الف</span>
               </div>
             </div>
 
             <div class="card-body">
-              <h5 class="card-title"><a href="#" data-toggle="modal" data-target=${"#" + id}>${data.title}<span
+              <h5 class="card-title"><a href="#" data-toggle="modal" data-target=${"#" + id}>${recipe.title}<span
                     class=" glyphicon glyphicon-tower"></a></h5>
 
 
               <!--end section pay room -->
 
-              <p class="card-text">${data.discreption}</p>
+              <p class="card-text">${recipe.description}</p>
               <button class="btn btn-primary btn-bg" href="showPay" data-toggle="modal" data-target='#myModal'>الشراء
                 الان</button>
             </div>
@@ -46,13 +58,13 @@ const addCard = (data, id) => {
                   <!-- Wrapper for slides -->
                   <div class="carousel-inner">
                     <div class="item active">
-                      <img src=${data.urls[1]} alt="Los Angeles" style="max-height: 480px;">
+                      <img src=https:${recipe.fields.images[1]} alt="Los Angeles" style="max-height: 480px;">
                     </div>
                     <div class="item">
-                      <img src=${data.urls[2]} alt="Chicago" style="max-height: 480px;">
+                      <img src=https:${recipe.fields.images[2]} alt="Chicago" style="max-height: 480px;">
                     </div>
                     <div class="item">
-                      <img src=${data.urls[3]} alt="New York" style="max-height: 480px;">
+                      <img src=https:${recipe.fields.images[3]} alt="New York" style="max-height: 480px;">
                     </div>
                   </div>
                   <!-- Left and right controls -->
@@ -79,9 +91,13 @@ const addCard = (data, id) => {
 
         
 `;
-  cardContainer.innerHTML += newCard;
-};
-
+      cardContainer.innerHTML += newCard;
+      console.log(recipe);
+    } else {
+      console.log("nothing");
+    }
+  });
+});
 const removeCard = id => {
   const newCard = `
   <!-- start king room -->
